ef4e173b394c8c5b7d227f5f4c1a65ae1dc1cc4e
dmaslim <dmaslim@yahoo.com>
2021-07-09 11:56:22 +0700

    Added Products table

---
+++ Ze-Clowns/Ze-Clowns-SQL/Tables/Products.sql
@@ -0,0 +1,26 @@
+﻿
+--1. ProductID int
+--2. ProductName varchar(200)
+--3. ProductDescription varchar(500)
+--4. ProductActive bit
+--5. AddedBy varchar(10)
+--6. UpdatedBy varchar(10)
+--7. DateAdded datetime
+--8. DateUpdated datetime
+
+use Testingdb;
+
+CREATE TABLE [dbo].[Products]
+(
+	[ProductID] INT NOT NULL PRIMARY KEY,
+	ProductName varchar(200),
+	ProductDescription varchar(500),
+	Active bit,
+	Addedby varchar(10),
+	Updatedby varchar(10),
+	DateAdded datetime,
+	DateUpdated datetime
+
+);
+
+
