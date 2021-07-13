4fd4e1e8fb042c5ede0a65c2a0343bf67a3366b3
dmaslim <dmaslim@yahoo.com>
2021-07-13 11:41:11 +0700

    Added Departments table

---
+++ Ze-Clowns/Ze-Clowns-SQL/Tables/Departments.sql
@@ -0,0 +1,17 @@
+﻿
+--1. DepartmentID smallint PK
+--2. Name varchar(50) not null
+--3. GroupName varchar(50) not null
+--4. DateAdded datetime not null
+--5. DateUpdated datetime
+
+
+
+create table Departments (
+	DepartmentID smallint primary key,
+	[Name] varchar(50),
+	groupName varchar(50) not null,
+	DateAdded datetime not null,
+	DateUpdated datetime
+);
+
